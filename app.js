5d4bcdeddb75eff3a65198df69d74d6d4965ba97
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-10 08:48:54 -0500

    use inquirer to add questions

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i <profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
     }
-    console.log('================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+  ])
+  .then(answers => console.log(answers));
 
-printProfileData(profileDataArgs);
+
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template.js');
+
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
