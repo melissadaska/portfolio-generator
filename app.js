c46332a03c71a52bd90b90d0a592a3ed4c7f3020
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-14 08:14:30 -0500

    finish site

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { writeFile,copyFile} = require('./utils/generate-site.js');
+
 const generatePage = require('./src/page-template');
 
 const fs = require('fs');
@@ -125,14 +127,35 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-     if (err) throw new Error(err);
-
-     console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+//   fs.writeFile('./dist/index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Page created! Check out index.html in this directory to see it!');
+//     fs.copyFile('./src/style.css', './dist/style.css', err => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log('Style sheet copied successfully!');
+//     });
+//   });
+// });
 
 
 // const fs = require('fs');
